f17050440a647aafdc80af5820a2036e0e84bb54
Nidhi Salunke <110630480+NidhiSal@users.noreply.github.com>
2022-12-26 13:30:21 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
-import React from 'react'
-import '../styles/App.css';
+import React, { useState} from "react";
+import "../styles/App.css";
 const App = () => {
+  const [clockState, setClockState] = useState();
+
+  setInterval(() => {
+    let date = new Date()
+    setClockState(date.toLocaleString());
+  }, 1000);
+
 
   return (
     <div id="main">
-      <div className="date-time"></div>
+      <div className="date-time">{clockState}</div>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
